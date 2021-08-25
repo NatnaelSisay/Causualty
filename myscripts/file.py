748aab38b430c6842afea8ff9a9e4682609080f4
NatnaelSisay <natyjava8@gmail.com>
2021-08-25 11:08:38 +0300

    added logger file

--- myscripts/file.py
+++ myscripts/file.py
@@ -1,8 +1,12 @@
 import pandas as pd
+from my_logger import logger
+
 def read_csv(file_name):
   try:
+    logger.info(f"{file_name} : Reading")
     null_values = ["n/a", "na", "undefined"]
     df = pd.read_csv(f'../data/{file_name}', na_values=null_values)
+    logger.info("Finished Reading File!")
     return df
   except:
-    print("Log:-> File Not Found")
+    logger.error(f"File Not Found : {file_name}")
